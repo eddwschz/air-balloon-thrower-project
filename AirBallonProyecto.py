1b4d4790ab8f9096a171a4fc243ed72a6fa72bb3
eddwschz <eddwsanchez@gmail.com>
2017-11-29 22:38:06 -0600

    Avance.

--- AirBallonProyecto.py
+++ AirBallonProyecto.py
@@ -4,24 +4,60 @@ from OpenGL.GL import *
 import sys
 import OBJLector as lobj
 import random
+import bezierCurve as bC
 
 name = 'glut'
 angulo = 0.0
+
+#PERSPECTIVE
 eye = [0.0, 0.0, 300.0]
 camera = [0.0, 0.0, 0.0]
 
-light_ambient = [0.0, 0.0, 0.0, 1.0]
+#LIGTH
+light_ambient = [1.0, 1.0, 1.0, 1.0]
 mat_ambient = [0.8, 0.8, 0.8, 1.0]
 
 light_diffuse = [0.0, 1.0, 0.0, 1.0]
 light_position = [1.0, 0.0, 0.0, 1.0]
 mat_diffuse = [0.8, 0.8, 0.8, 1.0]
 
+#TIMER
 timer_secs = 100
+
+#LIST AIRBALLON GL
 air_ballon = None
+mountain = None
+
 angle = 0.0
 
+#OBJ AIR BALLON
 air_ballonOBJ = lobj.OBJLoader("AirBallonsEddie.obj")
+mountainOBJ = lobj.OBJLoader("MountainEddie.obj")
+
+#INDEX OF CURVE BEZIER
+index_curve = 0
+#INDEX OF POINT IN A CURVE BEZIER
+index_point_curve = 0
+
+#VARIABLES X Y Z
+xV = 0.0
+yV = 0.0
+zV = 0.0
+
+#CURVES
+curvesBezier = []
+print("CURVE A COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(0.0,-0.0,0.0),50.0,50.0,0.0,0.1))
+print("CURVE B COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(curvesBezier[0].controlPoints[8].x,0.0,curvesBezier[0].controlPoints[8].z),0.0,50.0,-50.0,0.1))
+print("CURVE C COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(curvesBezier[1].controlPoints[8].x,0.0,curvesBezier[1].controlPoints[8].z),-50.0,50.0,0.0, 0.1))
+print("CURVE D COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(curvesBezier[2].controlPoints[8].x,0.0,curvesBezier[2].controlPoints[8].z),-50.0,50.0,0.0, 0.1))
+print("CURVE E COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(curvesBezier[3].controlPoints[8].x,0.0,curvesBezier[3].controlPoints[8].z),0.0,50.0,50.0, 0.1)) 
+print("CURVE F COORDS")
+curvesBezier.append(bC.BezierCurve(bC.Vertice(curvesBezier[4].controlPoints[8].x,0.0,curvesBezier[4].controlPoints[8].z),50.0,50.0,0.0,0.1))
 
 def listObjects():
     global air_ballonOBJ
@@ -39,10 +75,29 @@ def listObjects():
     glEnd()
     glEndList();
 
+    global mountainOBJ
+    global mountain
+    mountain = glGenLists(1);
+    
+    glNewList(mountain, GL_COMPILE);
+    glBegin(GL_TRIANGLES) 
+    for o in mountainOBJ.objetos:
+        for c in o.caras:
+            #glColor3f(0.184, 0.107, 0.043)
+            glColor3f(random.uniform(0,1), random.uniform(0,1), random.uniform(0,1))
+            glVertex3f(c.vertices[0].x,c.vertices[0].y, c.vertices[0].z)
+            glVertex3f(c.vertices[1].x,c.vertices[1].y, c.vertices[1].z)
+            glVertex3f(c.vertices[2].x,c.vertices[2].y, c.vertices[2].z)
+    glEnd()
+    glEndList();
 
 def airBallon():
     global air_ballon  
     glCallList(air_ballon)
+
+def mountainW():
+    global mountain
+    glCallList(mountain)
 
 
 def main():
@@ -73,7 +128,7 @@ def main():
     glutDisplayFunc(display)
     glutReshapeFunc(reshape)
     glutKeyboardFunc(keyboard)
-    glutTimerFunc(timer_secs, movimiento, 0)
+    glutTimerFunc(timer_secs, movement, 0)
 
     listObjects()
 	
@@ -93,28 +148,37 @@ def reshape(w, h):
 
 def display():
     #	reshape(300, 300)
-    global angle
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    #global angle
+    global xV
+    global yV
+    global zV
 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     gluLookAt(eye[0], eye[1], eye[2], camera[0],camera[1], camera[2], 0.0, 1.0, 0.0)
-    
-    
-    
+
     glPushMatrix()
 
-    
-
-    
+    #glRotatef(angle, 0.0, 1.0, 0.0)
+    glTranslatef(xV, yV, zV)
+    if curvesBezier[index_curve].curvasPoints[index_point_curve].z<0:
+        glScalef(curvesBezier[index_curve].curvasPoints[index_point_curve].z*-0.5/10.0, curvesBezier[index_curve].curvasPoints[index_point_curve].z*-0.5/10.0, curvesBezier[index_curve].curvasPoints[index_point_curve].z*-0.5/10.0)
+        print("Llega hasta " + str(curvesBezier[index_curve].curvasPoints[index_point_curve].z*-0.5/10.0))
+    else:
+       glScalef(10.0,10.0,10.0) 
+        
+    airBallon()
 
     glPopMatrix()
-    glRotatef(angle, 0.0, 1.0, 0.0)
-    glTranslatef(200.0, 0.0, 0.0)
-    glScalef(20.0, 20.0, 20.0)
-    airBallon()
+ 
+    glTranslatef(0.0,0.0,0)
+    glScalef(100.0, 100.0, 100.0)
+    mountainW()
     
+
+    print("DRAW")
 
     glFlush()
     glutSwapBuffers()
@@ -124,12 +188,31 @@ def keyboard(key, x, y):
     if ord(key) == 27:
         sys.exit(0)
 
-def movimiento(value):
+def movement(value):
+    global xV
+    global yV
+    global zY
+    global index_curve
+    global index_point_curve
+    global curvesBezier
+    glutTimerFunc(timer_secs, movement, 0)
 
-    global angle 
+    if index_point_curve<10:
+        index_point_curve=index_point_curve+1
+    else:
+        index_point_curve=0
+        if(index_curve<5):
+            index_curve=index_curve+1
+        else:
+            index_curve=0
     
-    glutTimerFunc(timer_secs, movimiento, 0)
-    angle= angle + (10.0 if angle < 360.0 else -360.0)
+    xV = curvesBezier[index_curve].curvasPoints[index_point_curve].x
+    yV = curvesBezier[index_curve].curvasPoints[index_point_curve].y
+    zV = curvesBezier[index_curve].curvasPoints[index_point_curve].z
+
+    print("In the curve " + str(index_curve) + " in the point of curve " + str(index_point_curve))
+    print(" X: " + str(xV) + " " + " Y: " + str(yV) + " Z: " + str(zV))
+
     glutPostRedisplay()
 
 if __name__ == '__main__':
--- bezierCurve.py
+++ bezierCurve.py
@@ -3,43 +3,58 @@ import math
 import numpy as np
 
 class Vertice():
-    def __init__(self, xValue, yValue):
+    def __init__(self, xValue, yValue, zValue):
         self.x=xValue
         self.y=yValue
-
-class ControlPoints():
-    def __init__(self, vA):
-        self.puntosControl = self.calculatePC(vA)
-    
-    def calculatePC(self,vI):
-        puntosControl=[]
-        d=10
-        puntosControl.append(vI)
-        puntosControl.append(Vertice(vI.x+d*2,vI.y+d))
-        puntosControl.append(Vertice(vI.x+d*4,vI.y))
-        puntosControl.append(Vertice(vI.x+d*6,vI.y-d))
-        return puntosControl
+        self.z=zValue
 
 class BezierCurve():
-    def __init__(self, pC, dtV):
-        self.puntosControl=pC
-        self.curva =[]
+    def __init__(self, vAct, xInc, yInc, zInc, dtV):
+        self.vA = vAct
+        self.xI = xInc
+        self.yI = yInc
+        self.zI = zInc
+        self.controlPoints=[]
+        self.curvasPoints=[]
         self.dt = dtV
+        self.calculateCurveControlPoints()
         self.calculateCurve()
+
+    def calculateCurveControlPoints(self):
+        #5 CONTROL POINTS FOR A CURVE
+        print("CONTROL POINTS FOR A CURVE")
+        self.controlPoints.append(self.vA)
+        print("P1:" + " X: "+ str(self.controlPoints[0].x) + " Y: "+ str(self.controlPoints[0].y) + " Z: "+ str(self.controlPoints[0].z))
+        self.controlPoints.append(Vertice(self.controlPoints[0].x+self.xI, self.controlPoints[0].y-self.yI , self.controlPoints[0].z+self.zI))
+        print("P2:" + " X: "+ str(self.controlPoints[1].x) + " Y: "+ str(self.controlPoints[1].y) + " Z: "+ str(self.controlPoints[1].z))
+        self.controlPoints.append(Vertice(self.controlPoints[1].x+self.xI, self.controlPoints[1].y-self.yI , self.controlPoints[1].z+self.zI))
+        print("P3:" + " X: "+ str(self.controlPoints[2].x) + " Y: "+ str(self.controlPoints[2].y) + " Z: "+ str(self.controlPoints[2].z))
+        self.controlPoints.append(Vertice(self.controlPoints[2].x+self.xI, self.controlPoints[2].y-self.yI , self.controlPoints[2].z+self.zI))
+        print("P4:" + " X: "+ str(self.controlPoints[3].x) + " Y: "+ str(self.controlPoints[3].y) + " Z: "+ str(self.controlPoints[3].z))
+        self.controlPoints.append(Vertice(self.controlPoints[3].x+self.xI, self.controlPoints[3].y-self.yI , self.controlPoints[3].z+self.zI))
+        print("P5:" + " X: "+ str(self.controlPoints[4].x) + " Y: "+ str(self.controlPoints[4].y) + " Z: "+ str(self.controlPoints[4].z))
+        self.controlPoints.append(Vertice(self.controlPoints[4].x+self.xI, self.controlPoints[4].y+self.yI , self.controlPoints[4].z+self.zI))
+        print("P6:" + " X: "+ str(self.controlPoints[5].x) + " Y: "+ str(self.controlPoints[5].y) + " Z: "+ str(self.controlPoints[5].z))
+        self.controlPoints.append(Vertice(self.controlPoints[5].x+self.xI, self.controlPoints[5].y+self.yI , self.controlPoints[5].z+self.zI))
+        print("P7:" + " X: "+ str(self.controlPoints[6].x) + " Y: "+ str(self.controlPoints[6].y) + " Z: "+ str(self.controlPoints[6].z))
+        self.controlPoints.append(Vertice(self.controlPoints[6].x+self.xI, self.controlPoints[6].y+self.yI , self.controlPoints[6].z+self.zI))
+        print("P8:" + " X: "+ str(self.controlPoints[7].x) + " Y: "+ str(self.controlPoints[7].y) + " Z: "+ str(self.controlPoints[7].z))
+        self.controlPoints.append(Vertice(self.controlPoints[7].x+self.xI, self.controlPoints[7].y+self.yI , self.controlPoints[7].z+self.zI))
+        print("P9:" + " X: "+ str(self.controlPoints[8].x) + " Y: "+ str(self.controlPoints[8].y) + " Z: "+ str(self.controlPoints[8].z))
 
     def calculateCurve(self):
         j=0
         for i in range(int(1/self.dt)+1):
-            print("En dt= " + str(self.dt*i))
+            print("     EN DT= " + str(self.dt*i))
             self.pointsOnCurveBezier(self.dt*j)
             j=j+1
 
     def pointsOnCurveBezier(self,t):
-        rX = ((1-t)**3)*self.puntosControl[0].x + 3*t*((1-t)**2)*self.puntosControl[1].x + 3*(t**2)*(1-t)*self.puntosControl[2].x + (t**3)*self.puntosControl[3].x
-        rY = ((1-t)**3)*self.puntosControl[0].y + 3*t*((1-t)**2)*self.puntosControl[1].y + 3*(t**2)*(1-t)*self.puntosControl[2].y + (t**3)*self.puntosControl[3].y
-        print("x: " + str(rX) + " " + "y: " + str(rY))
-        result = Vertice(rX,rY)
-        self.curva.append(result)
+        rX = ((1-t)**3)*self.controlPoints[0].x + 3*t*((1-t)**2)*self.controlPoints[2].x + 3*(t**2)*(1-t)*self.controlPoints[4].x + (t**3)*self.controlPoints[8].x
+        rY = ((1-t)**3)*self.controlPoints[0].y + 3*t*((1-t)**2)*self.controlPoints[2].y + 3*(t**2)*(1-t)*self.controlPoints[4].y + (t**3)*self.controlPoints[8].y
+        rZ = ((1-t)**3)*self.controlPoints[0].z + 3*t*((1-t)**2)*self.controlPoints[2].z + 3*(t**2)*(1-t)*self.controlPoints[4].z + (t**3)*self.controlPoints[8].z
+        print("     X: " + str(rX) + " Y: " + str(rY) + " Z: " + str(rZ))
+        self.curvasPoints.append(Vertice(rX,rY,rZ))
 
     
 
